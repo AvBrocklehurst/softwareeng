d62fe1c29215ffeaafd7a14af5c7753fdeca8029
Adam Brocklehurst <Adam Brocklehurst>
2016-02-01 14:04:16 +0000

    Adams new OS test

--- softwaredev/src/softwaredev/Test.java
+++ softwaredev/src/softwaredev/Test.java
@@ -3,7 +3,7 @@ package softwaredev;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("matt's one works");
+		System.out.println("Adam's new test is better");
 	}
 
 }
