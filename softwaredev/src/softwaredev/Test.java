2422c5b74ec3eeedfb57f4815c66b4bb9f69768f
finalMattG <matt.99@ntlworld.com>
2016-02-01 16:26:32 +0000

    Commit from within Eclipse

--- softwaredev/src/softwaredev/Test.java
+++ softwaredev/src/softwaredev/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Adam's new test is better"); //subjective
 		System.out.println("Testing");
+		System.out.println("Testing Ctrl+# from Eclipse");
 	}
-
 }
