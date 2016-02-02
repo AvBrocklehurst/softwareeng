4706006d759dc98d50e9a5441d0b828b03ad1be6
joshie77 <joshholmes25@gmail.com>
2016-02-02 16:37:27 +0000

    Made a quick start on the MIDI interface and class that implements it

--- softwaredev/src/simori/MIDIPlayer.java
+++ softwaredev/src/simori/MIDIPlayer.java
@@ -1,9 +1,38 @@
 package simori;
+
+import javax.sound.midi.Synthesizer;
+
 /**
  * 
  * @author Josh
+ * @version 1.0.0
+ * 
+ * Interface that describes the public methods available to accessing and playing MIDI sounds
  *
  */
 public interface MIDIPlayer {
+	
+	/**
+	 * @author Josh
+	 * @param None 
+	 * @return Synthesizer
+	 * @throws MidiUnavailableException
+	 * 
+	 * Produces a MIDI synthesizer (for system to use).
+	 * 
+	 */
+	public Synthesizer getSynthesizer();
+	
+	/**
+	 * @author Josh
+	 * @param notes
+	 * @return void
+	 * 
+	 * Method will take a list of all notes (where a note consists of a channel, instrument, pitch and velocity)
+	 * Method will play all notes simulatenously(or as close to simulatneous as possible)
+	 */
+	public void playNotes(int[][] notes);
+	
+	
 
 }
