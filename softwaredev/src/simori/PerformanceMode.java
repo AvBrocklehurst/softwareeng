2b43f91c720cbe86830df5402c3741c230dbc1f3
dupeydoo <jrmd202@exeter.ac.uk>
2016-02-02 16:08:49 +0000

    Pesudocode switch for shoulder buttons

--- softwaredev/src/simori/PerformanceMode.java
+++ softwaredev/src/simori/PerformanceMode.java
@@ -1,7 +1,23 @@
 package simori;
 
-
+/**
+ * The class for Performance Mode, extending
+ * the abstract class Mode.
+ * 
+ * @author James
+ * @version 1.0.0
+ */
 
 public class PerformanceMode extends Mode {
+	
+	/**
+	 * Gets the current mode.
+	 * 
+	 * @author James
+	 * @version 1.0.0
+	 */
+	public void getMode(){
+		
+	}
 
 }
