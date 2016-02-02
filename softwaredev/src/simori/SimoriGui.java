8490fef2f1e24978830a34436032cb2495a61715
finalMattG <matt.99@ntlworld.com>
2016-02-02 16:23:48 +0000

    Added SimoriGui (currently empty) and SimoriGuiEvents with EventObjects and Listener interfaces

---
+++ softwaredev/src/simori/SimoriGui.java
@@ -0,0 +1,8 @@
+package simori;
+import java.util.EventObject;
+
+import javax.swing.JPanel;
+
+public class SimoriGui extends JPanel {
+	
+}
---
+++ softwaredev/src/simori/SimoriGuiEvents.java
@@ -0,0 +1,64 @@
+package simori;
+import java.util.EventObject;
+
+public class SimoriGuiEvents {
+	
+	public class GridButtonEvent extends EventObject {
+		
+		private int x, y;
+		private SimoriGui src;
+		
+		public GridButtonEvent(SimoriGui src, int x, int y) {
+			super(src);
+			this.src = src;
+			this.x = x;
+			this.y = y;
+		}
+		
+		public int getX() {
+			return x;
+		}
+		
+		public int getY() {
+			return y;
+		}
+		
+		public SimoriGui getSource() {
+			return src;
+		}
+	}
+	
+	public class FunctionButtonEvent extends EventObject {
+		
+		private FunctionButton btn;
+		private SimoriGui src;
+		
+		public FunctionButtonEvent(SimoriGui src, FunctionButton btn) {
+			super(src);
+			this.src = src;
+			this.btn = btn;
+		}
+		
+		public FunctionButton getFunctionButton() {
+			return btn;
+		}
+		
+		public SimoriGui getSource() {
+			return src;
+		}
+	}
+	
+	public enum FunctionButton {
+		L1, L2, L3, L4, R1, R2, R3, R4, POWER, OK
+	}
+	
+	public interface GridButtonListener {
+		
+		public void onGridButtonPress(GridButtonEvent e);
+	}
+	
+	public interface FunctionButtonListener {
+		
+		public void onFunctionButtonPress(FunctionButtonEvent e);
+	}
+}
