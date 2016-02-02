bd556fde5dc02c0dfe6cc39a6758e239dcf1ee18
dupeydoo <jrmd202@exeter.ac.uk>
2016-02-02 15:14:30 +0000

    Beginning Abstract Mode

---
+++ softwaredev/src/simori/Mode.java
@@ -0,0 +1,12 @@
+package simori;
+
+/**
+ * An abstract class defining methods for general
+ * use in the Mode subclasses.
+ * 
+ * @author James
+ * @version 1.0.0
+ */
+public abstract class Mode {
+
+}
