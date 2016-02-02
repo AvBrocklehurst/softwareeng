9742d888465b9b2b43e03ea9a68d8a9a19d775e4
Jurek Skoczylas <Jurek Skoczylas>
2016-02-02 12:21:58 +0000

    Adding some Clock stub methods

--- softwaredev/src/simori/Clock.java
+++ softwaredev/src/simori/Clock.java
@@ -3,10 +3,33 @@ package simori;
 	/**
 	 * 
 	 * @author Jurek
-	 * @version 1.0.0
+	 * @version 1.0.1
 	 *
 	 */
 
-public class Clock {
+public class Clock implements Runnable {
 
+		/**
+		 * The thread method for running the clock
+		 * @author Jurek
+		 * @version 1.0.1
+		 */
+		@Override
+		public void run() {
+			while(true){
+				
+			}
+		}
+		
+		/**
+		 * Stops the execution of the thread
+		 * @author Jurek
+		 * @version 1.0.1
+		 */
+		public void stop() {
+			
+		}
+	
+	
+	
 }
