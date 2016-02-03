4a616466a7404056a18c22cc33e31abbc578fd04
joshie77 <joshholmes25@gmail.com>
2016-02-03 12:43:37 +0000

    I just have 2

---
+++ softwaredev/src/simori/Exceptions/InvalidMIDIChannelException.java
@@ -0,0 +1,12 @@
+package simori.Exceptions;
+
+public class InvalidMIDIChannelException {
+    
+	public InvalidMIDIChannelException() {}
+
+    //Constructor that accepts a message
+    public InvalidMIDIChannelException(String message)
+    {
+       super(message);
+    }
+}
---
+++ softwaredev/src/simori/Note.java
@@ -0,0 +1,16 @@
+package simori;
+
+public class Note {
+	private int channel;
+	private int instrument;
+	private int pitch;
+	private int velocity;
+	
+	
+	public Note(int channel, int instrument, int pitch, int velocity ) {
+		if(!(0<channel && channel<= 15)){throw new 
+		
+	}
+	
+
+}
