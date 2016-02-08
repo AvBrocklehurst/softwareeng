ff5a63c7dfd96c97acd385cc98bd49409ef33193
Adam Brocklehurst <avbrocklehurst@hotmail.co.uk>
2016-02-08 19:30:49 +0000

    matrix tests done

--- softwaredev/src/simori/Tests/TestMatrixModel.java
+++ softwaredev/src/simori/Tests/TestMatrixModel.java
@@ -2,6 +2,8 @@
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -50,6 +52,34 @@ public class TestMatrixModel {
 	public void updateButtonTest() throws InvalidCoordinatesException{
 		model.updateButton((byte) 0, (byte)0, (byte)0);
 		assertEquals(true, model.getCol((byte)0, (byte)0)[0]);
+	}
+	
+	@Test
+	public void getLayersTest(){
+		model.setChannel((byte) 1, (byte)12);
+		List<Byte> layers = model.getLayers();
+		assertEquals(0, layers.get(0).byteValue());
+		assertEquals(1, layers.get(1).byteValue());
+	}
+	
+	@Test
+	public void gridTest() throws InvalidCoordinatesException{
+		model.updateButton((byte) 0,(byte)0, (byte)0);
+		model.updateButton((byte) 0,(byte)2, (byte)2);
+		model.updateButton((byte) 0,(byte)4, (byte)4);
+		model.updateButton((byte) 0,(byte)6, (byte)6);
+		model.updateButton((byte) 0,(byte)8, (byte)8);
+		model.updateButton((byte) 0,(byte)10,(byte) 10);
+		model.updateButton((byte) 0,(byte)12,(byte) 12);
+		model.updateButton((byte) 0,(byte)14, (byte)14);
+		boolean[][] col = model.getGrid((byte)0);
+		for(int x = 0; x < 16; x++){
+			if(x % 2 == 0){
+				assert(col[x][x]);
+			} else {
+				assert(!col[x][x]);
+			}
+		}
 	}
 
 }
