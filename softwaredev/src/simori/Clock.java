791bad8c160d98f5f2af4e421d7e4353f21c6ff6
Jurek Skoczylas <Jurek Skoczylas>
2016-02-10 11:08:50 +0000

    JavaDocs, comments and versioning

--- softwaredev/src/simori/Clock.java
+++ softwaredev/src/simori/Clock.java
@@ -11,12 +11,15 @@ import javax.sound.midi.MidiUnavailableException;
 import simori.Exceptions.InvalidCoordinatesException;
 
 	/**
-	 * 
+	 * Class implementing Runnable which keeps track of the current tempo and plays notes which are currently active
 	 * @author Jurek
 	 * @author Adam
-	 * @version 1.1.1
+	 * @version 1.2.0
 	 *
 	 */
+	//TODO different loop breakpoints
+	//TODO error checking for midi.play(layers)
+	//TODO fine-tune the processing so that all the notes from the next column are played
 
 public class Clock implements Runnable {
 		private boolean running = true;
@@ -31,25 +34,34 @@ public class Clock implements Runnable {
 		/**
 		 * Constructor for the class
 		 * @author Jurek
-		 * @version 1.0.3
+		 * @version 1.0.4
 		 * @param model Holds the reference to the MatrixModel
+		 * @param midi Holds the reference to the MIDIPlayer
+		 * @param mode Holds the reference to the GUI
+		 * @param bbm Beats Per Minute; used to calculate the period
 		 */
 		Clock(MatrixModel model, MIDIPlayer midi, PerformanceMode mode, float bbm){
 			this.model = model;
 			this.midi = midi;
 			this.mode = mode;
 			lock = new Object();
+			//converts the beats per minute into the period, i.e.: how many seconds to play the notes
 			period = (long)((1f/(bbm/60f))*1000f);
 		}
 	
 		/**
 		 * The thread method for running the clock
+		 * First sets up a timer, before entering the proper thread loop, where it
+		 * continuously takes data from the model, processes it and sends it across
+		 * to the MIDIPlayer to be played. Highlights the current column on the GUI using
+		 * the mode.
 		 * @author Jurek
 		 * @author Adam
-		 * @version 1.1.1
+		 * @version 1.2.0
 		 */
 		@Override
 		public void run() {
+			//starts a secondary thread to keep track of the tempo
 			Timer timer = new Timer();
 			timer.scheduleAtFixedRate(timerTask = new TimerTask() {
 				  @Override
@@ -66,10 +78,14 @@ public class Clock implements Runnable {
 				List<Byte> activeLayers = model.getLayers();
 				byte[][] layers = new byte[activeLayers.size()][];
 				ArrayList<Byte> usedColumns = new ArrayList<Byte>();
+				//using arrays with an array,
+				//for each generally active layer...
 				for(byte x=0; x<(byte)activeLayers.size(); x++){
 					byte notZero = 0;
 					byte[] thisLayer = new byte[19];
+					//...get its current column...
 					boolean[] layer = model.getCol(activeLayers.get(x), currentColumn);
+					//...add any active notes to the current inner array...
 					for(byte y=0; y<layer.length; y++){
 						if(layer[y]){
 							thisLayer[y + 3] = (byte) (67 - y);
@@ -78,9 +94,11 @@ public class Clock implements Runnable {
 							thisLayer[y + 3] = 0;
 						}
 					}
+					//...discard unused columns and resize the inner arrays...
 					if(notZero > 0){
 						usedColumns.add(x);
 						layers[x] = new byte[notZero + 3];
+						//[Channel, Instrument, Velocity, Note, Note, Note...]
 						short instrument = model.getInstrument(activeLayers.get(x));
 						layers[x][1] = (byte) ((instrument < 128) ? instrument : instrument - 127);
 						layers[x][2] = model.getVelocity(activeLayers.get(x));
@@ -94,54 +112,37 @@ public class Clock implements Runnable {
 						}
 					}
 				}
+				//...discard unused layers and resize the outer array
 				byte[][] toBePlayed = new byte[usedColumns.size()][];
 				for (byte i=0; i<usedColumns.size(); i++){
 					toBePlayed[i] = layers[usedColumns.get(i)];
 				}
 				
+				//wait until the beat hits...
 				synchronized(lock){
 					try {
 						lock.wait();
 					} catch (InterruptedException e) {}
 				}
-				if(layers[0] != null)
-					try {
-						//TODO sprint2: error checking before play(layers) I (Josh) will do this
-						midi.play(layers);
-					} catch (InvalidMidiDataException e1) {e1.printStackTrace(); System.exit(1);}
+				//...and send a play request to the MIDIPlayer
+				//if MIDIPlayer throws an error, print it out and stop the JVM
+				if(layers[0] != null) try {midi.play(layers);} catch (InvalidMidiDataException e1) {e1.printStackTrace(); System.exit(1);}
+				//turn the lights on the current column
 				try{mode.tickerLight(currentColumn);} catch (InvalidCoordinatesException e) {}
 				
-				//15 will need to be replaced later
-				if(currentColumn == /*model.getLoop*/15){currentColumn = 0;}
+				//check if the loop needs to be restarted, otherwise just continue to the next column
+				if(currentColumn == 15){currentColumn = 0;}
 				else{currentColumn++;}
-				
-				//TODO GET A LIST OF LISTS FOR THE CURRENT COLUMN FROM MatrixModel-----------------------DONE
-				//TODO SEND LIST OF LISTS TO MidiPlayer---------------------DONE
-				//TODO SEND GUI A MESSAGE --------------------------------DONE
-				//TODO WAIT A SET OF TIME == THE TEMPO ------------------------ DONE
-				//TODO CHECK IF END OF COLUMN LOOP FOUND, IF SO currentColumn = 0, ELSE currentColumn += 1 ---------- SORT OF DONE, SINCE LOOP CHANGING IS NOT SET
-				
 			}
 		}
-		
-		public boolean contains(boolean[] layer){
-			for (boolean bool : layer) if (bool == true) return true;
-			return false;
-		}
-		
+				
 		/**
-		 * Stops the execution of the thread
+		 * Stops the execution of the thread and the timer
 		 * @author Jurek
-		 * @version 1.0.1
+		 * @version 1.0.2
 		 */
 		public void off() {
 			running = false;
 			timerTask.cancel();
-		}	
-		
-//		public static void main(String[] args){
-//			Clock clock = new Clock(new MIDISoundPlayer());
-//			Thread thread = new Thread(clock);
-//			thread.start();
-//		}
+		}
 }
---
+++ softwaredev/src/simori/Tests/TestClock.java
@@ -0,0 +1,24 @@
+package simori.Tests;
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestClock {
+
+	@Before
+	public void setUp() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void whatExactlyAmISuppposedToTest() {
+		
+	}
+	
+}
