503a8dd5b1600253c5186dcbd96a2c0a08e1e3c1
finalMattG <matt.99@ntlworld.com>
2016-02-10 13:05:20 +0000

    Javadoc'd SimoriGuiEvents

--- softwaredev/src/simori/SimoriGuiEvents.java
+++ softwaredev/src/simori/SimoriGuiEvents.java
@@ -3,13 +3,30 @@ import java.util.EventObject;
 
 import simori.Exceptions.InvalidCoordinatesException;
 
+/**
+ * Defines listener interfaces and event types
+ * for events passed from the GUI to the Mode.
+ * @author Matt
+ * @version 1.1.0
+ */
 public class SimoriGuiEvents {
 	
+	/**
+	 * Event generated when an LED button in the grid is pressed.
+	 * @see GridButtonListener
+	 * @author Matt
+	 * @version 1.0.1
+	 */
 	public static class GridButtonEvent extends EventObject {
 		
 		private int x, y;
 		private SimoriGui src;
 		
+		/**
+		 * @param src The SimoriGui the button belongs to
+		 * @param x Horizontal coordinate of button pressed
+		 * @param y Vertical coordinate of button pressed
+		 */
 		public GridButtonEvent(SimoriGui src, int x, int y) {
 			super(src);
 			this.src = src;
@@ -17,47 +34,71 @@ public class SimoriGuiEvents {
 			this.y = y;
 		}
 		
+		/** @return Horizontal grid coordinate of button pressed */
 		public int getX() {
 			return x;
 		}
 		
+		/** @return Vertical grid coordinate of button pressed */
 		public int getY() {
 			return y;
 		}
 		
+		/** @return GUI instance containing button pressed */
 		public SimoriGui getSource() {
 			return src;
 		}
 	}
 	
+	/**
+	 * Event generated when any button not part of the grid is pressed.
+	 * This includes ON, OK and the numbered L and R buttons.
+	 * @see FunctionButtonListener
+	 * @see FunctionButton
+	 * @author Matt
+	 * @version 1.2.0
+	 */
 	public static class FunctionButtonEvent extends EventObject {
 		
 		private FunctionButton btn;
 		private SimoriGui src;
 		
+		/**
+		 * @param src The GUI containing the button pressed
+		 * @param btn enum identifying button pressed
+		 */
 		public FunctionButtonEvent(SimoriGui src, FunctionButton btn) {
 			super(src);
 			this.src = src;
 			this.btn = btn;
 		}
 		
+		/** @return enum identifying button pressed */
 		public FunctionButton getFunctionButton() {
 			return btn;
 		}
 		
+		/** @return GUI instance containing button pressed */
 		public SimoriGui getSource() {
 			return src;
 		}
 	}
 	
+	/**
+	 * Enumerated class to distinguish types of button
+	 * @author Matt
+	 * @version 1.0.1
+	 */
 	public enum FunctionButton {
 		L1, L2, L3, L4, R1, R2, R3, R4, POWER, OK
 	}
 	
+	/** Listener interface for {@link GridButtonEvent} */
 	public interface GridButtonListener  {
 		public void onGridButtonPress(GridButtonEvent e) throws InvalidCoordinatesException;
 	}
 	
+	/** Listener interface for {@link FunctionButtonEvent} */
 	public interface FunctionButtonListener {
 		public void onFunctionButtonPress(FunctionButtonEvent e);
 	}
