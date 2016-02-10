4de40d07a85b742d456a8aa0c7b864585e2eace3
Josh Holmes <joshholmes25@gmail.com>
2016-02-10 22:21:26 +0000

    Update runtests.bat

--- runtests.bat
+++ runtests.bat
@@ -1,2 +1,2 @@
 cd softwareeng/src
-java -cp ".:junit-4.12.jar:hamcrest-core-1.3.jar:simori:simori/Tests" org.junit.runner.JUnitCore simori.Tests.TestSuite
+java -cp ".:../../junit-4.12.jar:../../hamcrest-core-1.3.jar:simori:simori/Tests" org.junit.runner.JUnitCore simori.Tests.TestSuite
