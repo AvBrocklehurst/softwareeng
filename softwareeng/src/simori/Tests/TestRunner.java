66a8f1a00c30a058f2cd17e84260c1662d971756
joshie77 <joshholmes25@gmail.com>
2016-02-12 15:26:58 +0000

    Added test Runner. This is the thing that is meant to produce Junit results

---
+++ softwareeng/src/simori/Tests/TestRunner.java
@@ -0,0 +1,29 @@
+package simori.Tests;
+
+
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+
+/**
+ * @author Josh
+ * @version 1.0.1
+ * class that is able to run the test suite with meaningful output
+ */
+public class TestRunner {
+	
+	public static void main(String[] args) {
+		Result result = JUnitCore.runClasses(TestSuite.class);
+		
+		for(Failure failure : result.getFailures()){
+			System.out.println(failure.toString());
+		}
+		
+		if(result.wasSuccessful()){
+			System.out.println("All tests were sucessful");
+		}
+	}
+
+}
