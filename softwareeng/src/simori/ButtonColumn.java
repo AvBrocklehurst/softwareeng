65e05ec1fbaefe6a4a38fe5310d983d184bd1d10
finalMattG <matt.99@ntlworld.com>
2016-02-16 12:26:28 +0000

    New files I forgot to track again :/

---
+++ softwareeng/src/simori/ButtonColumn.java
@@ -0,0 +1,10 @@
+package simori;
+
+import simori.SimoriGuiEvents.FunctionButton;
+
+public class ButtonColumn {
+	
+	public ButtonColumn(FunctionButton... buttons) {
+		
+	}
+}
---
+++ softwareeng/src/simori/OffMode.java
@@ -0,0 +1,27 @@
+package simori;
+
+import simori.SimoriGuiEvents.FunctionButton;
+import simori.SimoriGuiEvents.FunctionButtonEvent;
+import simori.SimoriGuiEvents.GridButtonEvent;
+import simori.Exceptions.InvalidCoordinatesException;
+
+public class OffMode extends Mode {
+	
+	Simori simori;
+
+	public OffMode(Simori simori) {
+		super(simori);
+		this.simori = simori;
+	}
+
+	@Override
+	public void onGridButtonPress(GridButtonEvent e)
+			throws InvalidCoordinatesException {}
+	
+	@Override
+	public void onFunctionButtonPress(FunctionButtonEvent e) {
+		if (e.getFunctionButton() == FunctionButton.POWER) {
+			simori.setOn(true);
+		}
+	}
+}
