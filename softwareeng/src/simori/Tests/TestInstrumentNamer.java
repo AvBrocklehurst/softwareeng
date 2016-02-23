5e116c60e5a675b59a1b03048ca2b46f0dfa8769
joshie77 <joshholmes25@gmail.com>
2016-02-23 19:02:33 +0000

    Junit InstrumentNamer

--- softwareeng/src/simori/Tests/TestInstrumentNamer.java
+++ softwareeng/src/simori/Tests/TestInstrumentNamer.java
@@ -16,15 +16,10 @@ import simori.InstrumentNamer;
  * JUnit tests for the InstrumentNamer Class.
  */
 public class TestInstrumentNamer {
-	// get instance once
-	// get instance twice
-	// get instrument once
-	// get instrument twice
-	// check I get right instrument
-	// check I get right percussion
 	// check zero doesn't work
 	
 	InstrumentNamer instrumentNamer;
+	String instrumentName;
 	
 
 	@Before
@@ -36,9 +31,79 @@ public class TestInstrumentNamer {
 		instrumentNamer = null;
 	}
 
-	
+	/**
+	 * @author Josh
+	 * @version 1.0.0
+	 * 
+	 * test to see if an instance can be created.
+	 */
 	@Test
-	public void testGetName() {
-		fail("Not yet implemented");
+	public void testGetInstanceOnce() {
+		instrumentNamer = InstrumentNamer.getInstance();
+		assertNotNull(instrumentNamer);
 	}
+	
+	/**
+	 * @author Josh
+	 * @version 1.0.0
+	 * 
+	 * test to see if an instance can be created, but not multiple times.
+	 */
+	@Test
+	public void testGetInstanceTwice() {
+		instrumentNamer = InstrumentNamer.getInstance();
+		instrumentNamer = InstrumentNamer.getInstance();
+		assertNotNull(instrumentNamer);
+	}
+	
+	/**
+	 * @author Josh
+	 * @version 1.0.0
+	 * 
+	 * test to see if we get the correct instrument
+	 */
+	@Test
+	public void testGetInstrument() {
+		instrumentName = InstrumentNamer.getInstance().getName(110);
+		assertEquals("Bagpipe", instrumentName);
+	}
+	
+	/**
+	 * @author Josh
+	 * @version 1.0.0
+	 * 
+	 * test to see if we get the correct percussion instrument
+	 */
+	@Test
+	public void testGetPercussionInstrument() {
+		instrumentName = InstrumentNamer.getInstance().getName(133);
+		assertEquals("Hand Clap", instrumentName);
+	}
+	
+	/**
+	 * @author Josh
+	 * @version 1.0.0
+	 * 
+	 * since there is no instrument zero, we shouldnt get a value
+	 */
+	@Test
+	public void testGetInstrumentZero() {
+		instrumentName = InstrumentNamer.getInstance().getName(0);
+		assertNull(instrumentName);
+	}
+	
+	/**
+	 * @author Josh
+	 * @version 1.0.0
+	 * 
+	 * since there is no instrument 500, we shouldnt get a value
+	 */
+	@Test
+	public void testGetInstrument500() {
+		instrumentName = InstrumentNamer.getInstance().getName(500);
+		assertNull(instrumentName);
+	}
+	
+	
+	
 }
