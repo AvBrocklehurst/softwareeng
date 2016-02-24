6db7d5dd4729a43ea27a689a821ff921e7db1fae
finalMattG <matt.99@ntlworld.com>
2016-02-24 16:01:28 +0000

    Update compiletests.bat

--- compiletests.bat
+++ compiletests.bat
@@ -1,2 +1,2 @@
 cd softwareeng/src
-javac -cp .:../../junit-4.12.jar:../../hamcrest-core-1.3.jar simori/Tests/TestSuite.java
+javac -cp .:../../junit-4.12.jar:../../hamcrest-core-1.3.jar simori/Tests/TestRunner.java
