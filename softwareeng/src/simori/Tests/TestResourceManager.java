1d6e186c9ab6582e0fd309dc0ed3ccb8e5a670a2
joshie77 <joshholmes25@gmail.com>
2016-03-04 13:13:23 +0000

    finished testing resource manager

--- softwareeng/src/simori/Tests/TestResourceManager.java
+++ softwareeng/src/simori/Tests/TestResourceManager.java
@@ -1,21 +1,43 @@
 package simori.Tests;
+
+import simori.ResourceManager;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import org.junit.Test;
+
 /**
- * @author josh
+ * @author Josh
  * @version 1.0.0
  * 
  * Junit tests for resource manager
  */
-import static org.junit.Assert.*;
-import org.junit.Test;
-
 public class TestResourceManager {
-
+	
 	/**
+	 * @author Josh
+	 * @verison 1.0.0
 	 * 
+	 * Test to see if resource manager cannot get a file that exists
 	 */
 	@Test
-	public void testGetResource() {
-		fail("Not yet implemented");
+	public void testGetResourceBad() {
+		File file = ResourceManager.getResource("test.bad");
+		assertFalse(file.exists());
+	}
+	
+
+	/**
+	 * @author josh
+	 * @verison 1.0.0
+	 * 
+	 * Test to see if resource manager can get a file that exists
+	 */
+	@Test
+	public void testGetResourceGood() {
+		File file = ResourceManager.getResource("instruments.csv");
+		assertNotNull(file);
+		assertTrue(file.exists());
 	}
 
 }
