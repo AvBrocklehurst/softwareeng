7ddd9f309595515464fc2c63e18110cba85f145d
finalMattG <matt.99@ntlworld.com>
2016-03-04 16:15:51 +0000

    Did some network stuff

--- softwareeng/src/simori/NetworkClient.java
+++ softwareeng/src/simori/NetworkClient.java
@@ -1,34 +1,24 @@
 package simori;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import org.omg.CORBA.portable.OutputStream;
-
 public class NetworkClient {
-	  final static int    PORT  =  20160;
-	  
-	  public static void main( String[] argv ) 
-	    throws IOException {
-		  
+	final static int    PORT  =  20160;
+	
+	public static void main(String[] args) throws IOException {
 		iterateOverIPRange(getIP().substring(0, getIP().indexOf('.',getIP().indexOf('.') + 1) + 1));
-	    
-	    
-	  }
+	}
 	  
-	  private static String getIP(){
+	private static String getIP(){
 		try {
 			return Inet4Address.getLocalHost().getHostAddress();
 		} catch (UnknownHostException e) {
@@ -39,29 +29,26 @@ public class NetworkClient {
 	  }
 	  
 	  private static void iterateOverIPRange(String ip) {
+		  MatrixModel model = new MatrixModel(16, 16); //TODO the actual model
+		  model.setBPM((short) 55); 
 		  outerloop:
-		    for(int j = 93; j < 256; j++){
+		    for(int j = 0; j < 256; j++){
 		    	for(int i = 0; i < 256; i++){
 			    	System.out.println("scanning " + ip + j + "." + i);
 			        try {
 			        	Socket socket = new Socket();
-			            socket.connect(new InetSocketAddress(ip + j + "." + i, PORT), 50);
-			            File file = new File("C:\\Recovery.txt");
-			            // Get the size of the file
-			            long length = file.length();
-			            byte[] bytes = new byte[16 * 1024];
-			            InputStream in = new FileInputStream(file);
+			            socket.connect(new InetSocketAddress(ip + j + "." + i, PORT), 500);
 			            OutputStream out = (OutputStream) socket.getOutputStream();
-			            int count;
-			            while ((count = in.read(bytes)) > 0) {
-			            	out.write(bytes, 0, count);
-			            }
-			            
-			            out.close();
-			            in.close();
+			            ObjectOutputStream serializer = new ObjectOutputStream(out);
+			            serializer.writeObject(model);
+			            serializer.close();
+			            out.close(); //was probably closed automatically when serializer was closed anyway
 			            socket.close();
 				     	break outerloop;
 			        } catch (Exception e){
+			        	if (i == 7) {
+			        		e.printStackTrace();
+			        	}
 			        	System.out.println("nope");
 			        }
 			    }
