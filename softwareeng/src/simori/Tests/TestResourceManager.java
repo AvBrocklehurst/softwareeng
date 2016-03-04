e20287be78527e38223e03f02dec4a21cc4f48b3
joshie77 <joshholmes25@gmail.com>
2016-03-04 12:21:21 +0000

    maaybe conflict?

---
+++ softwareeng/src/simori/Tests/TestResourceManager.java
@@ -0,0 +1,21 @@
+package simori.Tests;
+/**
+ * @author josh
+ * @version 1.0.0
+ * 
+ * Junit tests for resource manager
+ */
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestResourceManager {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testGetResource() {
+		fail("Not yet implemented");
+	}
+
+}
