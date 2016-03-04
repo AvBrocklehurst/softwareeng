cd7693c34795b607158451d35108ac788b9135af
Adam Brocklehurst <avbrocklehurst@hotmail.co.uk>
2016-03-04 12:17:41 +0000

    please work you ip searcher

--- softwareeng/src/simori/NetworkClient.java
+++ softwareeng/src/simori/NetworkClient.java
@@ -9,38 +9,71 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import org.omg.CORBA.portable.OutputStream;
 
 public class NetworkClient {
-	final static String HOST  =  "127.0.0.1"; /* localhost */
 	  final static int    PORT  =  20160;
-
+	  
 	  public static void main( String[] argv ) 
 	    throws IOException {
-
-	    Socket s = new Socket( HOST, PORT );
+		  
+		iterateOverIPRange(getIP().substring(0, getIP().indexOf('.',getIP().indexOf('.') + 1) + 1));
 	    
-	    InputStream  in   =  s.getInputStream();
-	    java.io.OutputStream out  =  s.getOutputStream();
-
-	    BufferedReader reader =
-	      new BufferedReader( new InputStreamReader( in ) );
-	    PrintWriter writer =
-	      new PrintWriter( out );
-
-	    writer.print( "GET / HTTP/1.1\r\n" );
-	    writer.print( "Host : " + HOST + "\r\n" );
-	    writer.print( "Content-Length : 0\r\n" );
-	    writer.print( "\r\n" );
-	    writer.flush();
-
-	    int c;
-	    while ( ( c = reader.read() ) != -1 ) {
-	      System.out.write( c );
-	    }
-
-	    s.close();
+	    
 	  }
+	  
+	  private static String getIP(){
+		try {
+			return Inet4Address.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	  }
+	  
+	  private static void iterateOverIPRange(String ip) {
+		  outerloop:
+		    for(int j = 0; j < 256; j++){
+		    	for(int i = 0; i < 256; i++){
+			    	System.out.println("scanning " + ip + j + "." + i);
+			        try {
+			        	Socket socket = new Socket();
+			            socket.connect(new InetSocketAddress(ip + j + "." + i, PORT), 50);
+			            socket.close();
+			            break outerloop;
+			        	// Socket s = new Socket( ip + j + "." + i, PORT );
+			            /*
+			        	 InputStream  in   =  s.getInputStream();
+				     	    java.io.OutputStream out  =  s.getOutputStream();
+		
+				     	    BufferedReader reader =
+				     	      new BufferedReader( new InputStreamReader( in ) );
+				     	    PrintWriter writer =
+				     	      new PrintWriter( out );
+		
+				     	    writer.print( "GET / HTTP/1.1\r\n" );
+				     	    writer.print( "Host : " + getIP() + "\r\n" );
+				     	    writer.print( "Content-Length : 0\r\n" );
+				     	    writer.print( "\r\n" );
+				     	    writer.flush();
+		
+				     	    int c;
+				     	    while ( ( c = reader.read() ) != -1 ) {
+				     	      System.out.write( c );
+				     	    }
+		
+				     	    s.close(); */
+			        } catch (Exception e){
+			        	System.out.println("nope");
+			        }
+			    }
+		    }
+		}
 }
