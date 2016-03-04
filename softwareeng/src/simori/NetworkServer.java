21b95eca755c117ecd5885e7dcbaa4278c7bc747
Adam Brocklehurst <avbrocklehurst@hotmail.co.uk>
2016-03-04 15:26:59 +0000

    server pls

--- softwareeng/src/simori/NetworkServer.java
+++ softwareeng/src/simori/NetworkServer.java
@@ -16,37 +16,11 @@ public class NetworkServer {
 
 	  public static void main( String[] argv ) throws IOException {
 		  ServerSocket serverSocket = null;
-		  System.out.println("Starting");
-		  serverSocket = new ServerSocket(20160);
-		  Socket socket = null;
-		  InputStream in = null;
-		  OutputStream out = null;
-		  
-		  try {
-			  socket = serverSocket.accept();
-		  } catch (IOException ex) {
-		     System.out.println("Can't accept client connection. ");
-		  }
-		  try {
-		      in = socket.getInputStream();
-		  } catch (IOException ex) {
-		       System.out.println("Can't get socket input stream. ");
-		  }
-		  try {
-		      out = new FileOutputStream("C:\\Recovery2.txt");
-		  } catch (FileNotFoundException ex) {
-		      System.out.println("File not found. ");
-		  }
-		  byte[] bytes = new byte[16*1024];
-		  int count;
-		  while ((count = in.read(bytes)) > 0) {
-		    	System.out.println("writitng");
-		        out.write(bytes, 0, count);
-		  }
-		  
-		  out.close();
-		  in.close();
-		  socket.close();
-		  serverSocket.close();
+
+		  	    while ( true ) {
+		  	      Socket s = serverSocket.accept();
+		  	      Thread t = new Thread( new Worker(serverSocket,s ) );
+		  	      t.start();
+		  	    }
+		  	  }
 	  }
-}
--- softwareeng/src/simori/Worker.java
+++ softwareeng/src/simori/Worker.java
@@ -1,41 +1,64 @@
 package simori;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Worker implements Runnable {
   private Socket s;
+  private ServerSocket ss;
 
-  Worker( Socket s ) {
+  Worker(ServerSocket ss, Socket s ) {
     this.s = s;
+    this.ss = ss;
   }
 
   public void run() {
-    try {
-      InputStream  in   =  s.getInputStream();
-      OutputStream out  =  s.getOutputStream();
+	  System.out.println("Starting");
+	  ServerSocket serverSocket = ss;
+	  Socket socket = s;
+	  InputStream in = null;
+	  OutputStream out = null;
+	  
 
-      BufferedReader reader =
-	new BufferedReader( new InputStreamReader( in ) ); 
-      PrintWriter    writer =
-        new PrintWriter( out );
-
-      String content = reader.readLine() + "\n";
-
-      writer.print( "HTTP/1.1 200 OK\r\n" );
-      writer.print( "Content-Type: text/plain\r\n" );
-      writer.print( "Content-Length: " + content.length() + "\r\n" );
-      writer.print( "\r\n" );
-      writer.print( content );
-      writer.flush();
-      s.close();
-    } catch ( IOException e ) {
-      System.out.println( e );
-    }
+	  try {
+	      in = socket.getInputStream();
+	  } catch (IOException ex) {
+	       System.out.println("Can't get socket input stream. ");
+	  }
+	  try {
+	      out = new FileOutputStream("C:\\Recovery2.txt");
+	  } catch (FileNotFoundException ex) {
+	      System.out.println("File not found. ");
+	  }
+	  byte[] bytes = new byte[16*1024];
+	  int count;
+	  try {
+		while ((count = in.read(bytes)) > 0) {
+		    	System.out.println("writitng");
+		        out.write(bytes, 0, count);
+		  }
+	} catch (IOException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	  
+	  try {
+		out.close();
+		in.close();
+		  socket.close();
+		  serverSocket.close();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  
   }
 }
