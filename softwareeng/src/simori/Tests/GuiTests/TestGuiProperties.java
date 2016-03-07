57df23a44801df5c3e7e23357fb959ebdd788ceb
finalMattG <matt.99@ntlworld.com>
2016-03-07 22:54:05 +0000

    Started testing GuiProperties

---
+++ softwareeng/src/simori/Tests/GuiTests/TestGuiProperties.java
@@ -0,0 +1,38 @@
+package simori.Tests.GuiTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGuiProperties {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRatioOf() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSizeFontTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFont() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIcon() {
+		fail("Not yet implemented");
+	}
+}
