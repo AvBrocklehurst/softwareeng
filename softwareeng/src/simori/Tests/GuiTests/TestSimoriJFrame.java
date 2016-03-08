033226f4022941df255b40ab7be2c10281e9cd1c
Jurek Skoczylas <jurekskoczylas@op.pl>
2016-03-08 18:40:46 +0000

    Stub TestSimoriJFrame

---
+++ softwareeng/src/simori/Tests/GuiTests/TestSimoriJFrame.java
@@ -0,0 +1,23 @@
+package simori.Tests.GuiTests;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Class that tests SimoriJFrame
+ * @author Jurek
+ *
+ */
+public class TestSimoriJFrame {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+	
+}
