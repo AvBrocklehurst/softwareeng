e97f2b416eec0d6bbe2f38ca64c91970a7ac9d00
finalMattG <matt.99@ntlworld.com>
2016-03-08 21:32:07 +0000

    Javadoc tweaks

--- softwareeng/src/simori/Exceptions/KeyboardException.java
+++ softwareeng/src/simori/Exceptions/KeyboardException.java
@@ -1,5 +1,11 @@
 package simori.Exceptions;
 
+/**
+ * Thrown by {@link simori.Modes.QwertyKeyboard} to indicate that the requested
+ * dimensions do not produce enough buttons to mimic a QWERTY-style keyboard.
+ * @author Matt
+ * @version 1.0.0
+ */
 public class KeyboardException extends Exception {
 	
 	//Parameterless Constructor
