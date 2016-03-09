e7dc832bd76ca14ee159ac95fbc9b5dcdccd1e29
dupeydoo <jrmd202@exeter.ac.uk>
2016-03-09 15:26:21 +0000

    Testsimori and main merged

--- softwareeng/src/simori/Tests/TestMain.java
+++ softwareeng/src/simori/Tests/TestMain.java
@@ -1,18 +1,53 @@
 package simori.Tests;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
 import javax.sound.midi.MidiUnavailableException;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import simori.Simori;
+import simori.Exceptions.KeyboardException;
 
 /**
  * Tests the main method and consequently Simori's constructor.
  * @author Matt
+ * @author James
  * @version 2.0.0
+ * @see Simori
  */
 public class TestMain {
-
+	
+	private Simori testsimori;
+	
+	@Before
+	public void setUp() throws MidiUnavailableException, KeyboardException, IOException{
+		testsimori = new Simori();
+	}
+	
+	@After
+	public void tearDown(){
+		testsimori = null;
+	}
+	
+	
+	/**
+	 * Tests an instance of Simori was correctly created.
+	 * 
+	 * @author James
+	 * @version 1.0.0
+	 * @see Simori
+	 */
+	@Test
+	public void testSimori(){
+		assertThat("A Simori was not created!", testsimori, instanceOf(Simori.class));
+	}
+	
 	/**
 	 * Runs the main method to check for exceptions.
 	 * Makes the code coverage report look better.
@@ -20,6 +55,5 @@ public class TestMain {
 	@Test
 	public void testMainMethod() throws MidiUnavailableException {
 		Simori.main(null);
-		System.exit(0);
 	}
 }
