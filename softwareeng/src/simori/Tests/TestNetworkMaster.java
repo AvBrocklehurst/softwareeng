6af4242867196f3f24a259220e9efa7e44d6d6c4
Jurek Skoczylas <jurekskoczylas@op.pl>
2016-03-18 14:37:03 +0000

    restoring adam's network master test

---
+++ softwareeng/src/simori/Tests/TestNetworkMaster.java
@@ -0,0 +1,34 @@
+	/**
+ * 
+ */
+package simori.Tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import simori.MatrixModel;
+import simori.ModeController;
+import simori.SimoriSoundSystem;
+import simori.Exceptions.KeyboardException;
+import simori.Modes.NetworkMaster;
+import simori.Modes.QwertyKeyboard;
+import simori.SwingGui.SimoriJFrame;
+
+/**
+ * @author adam
+ *
+ */
+public class TestNetworkMaster {
+
+	@Test
+	public void testRunning() throws KeyboardException{
+		MatrixModel model = new MatrixModel(16, 16);
+		QwertyKeyboard keyboard = new QwertyKeyboard((byte)16, (byte)16);
+		SimoriJFrame gui = new SimoriJFrame(keyboard);
+		SimoriSoundSystem player = new SimoriSoundSystem();
+		ModeController modes = new ModeController(gui, model, 20160);
+		modes.setOn(true);
+		modes.startNetworkMaster();
+	}
+}
