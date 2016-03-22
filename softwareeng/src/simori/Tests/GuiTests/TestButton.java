e2bdc2c493b9a0ccad661e70dcbdb47c239af6e5
dupeydoo <jrmd202@exeter.ac.uk>
2016-03-22 03:01:23 +0000

    Retest button

--- softwareeng/src/simori/Tests/GuiTests/TestButton.java
+++ softwareeng/src/simori/Tests/GuiTests/TestButton.java
@@ -2,6 +2,8 @@ package simori.Tests.GuiTests;
 
 import static org.junit.Assert.assertEquals;
 
+import java.awt.Dimension;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,7 +20,6 @@ import simori.Tests.GuiTests.MockSimoriJFrame.MockButton;
  * it is redundant to test some of the methods in this class.
  * 
  * @author James
- * @author Jurek
  * @version 1.0.2
  * @see Button.java
  *
@@ -43,15 +44,31 @@ public class TestButton {
 		testgui = null;
 	}
 	
-//	@Test
-//	public void test_resized(){
-//		testbutt.resized();
-//		assertEquals("Resized was not set to true!", true, testbutt.getResized());
-//	}
 	
 	@Test
 	public void test_set_get_Text(){
 		testbutt.setText("Hello World!");
 		assertEquals("The text should be Hello World!", "Hello World!", testbutt.getText());
 	}
+	
+	@Test
+	public void test_setDefiniteSize_getPreferredSize(){
+		testbutt.setDefiniteSize(new Dimension(5,5));
+		assertEquals("The size was not set correctly!", new Dimension(5,5), testbutt.getPreferredSize());
+	}
+	
+	@Test
+	public void test_setDefiniteSize_getMaximumSize(){
+		testbutt.setDefiniteSize(new Dimension(5,5));
+		assertEquals("The size was not set correctly!", new Dimension(5,5), testbutt.getMaximumSize());
+	}
+	
+	@Test
+	public void test_setDefiniteSize_getMinimumSize(){
+		testbutt.setDefiniteSize(new Dimension(5,5));
+		assertEquals("The size was not set correctly!", new Dimension(5,5), testbutt.getMinimumSize());
+	}
+	
+	
+	
 }
