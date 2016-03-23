77b05e0f3ec00addb0339f898317ec75629ce7ba
Jurek Skoczylas <js725@blue03.ex.ac.uk>
2016-03-23 15:27:10 +0000

    More shans

--- softwareeng/src/simori/Tests/TestExceptionManager.java
+++ softwareeng/src/simori/Tests/TestExceptionManager.java
@@ -114,7 +114,7 @@ public class TestExceptionManager {
 		
 	}
 	
-	//@Test
+	@Test
 	public void testUnnchaughtExceptionFatal() {
 		try{
 			exman.uncaughtException(thread, new SimoriFatalException());
